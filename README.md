ef8d56361a7fd34eb7882dee3ca9a94a0983c377
zzenonn <miguel.saavedra@obf.ateneo.edu>
2020-06-04 23:07:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lambda System Tools
+
+This is a collection of Lambda functions I use to automate part of my environment. They run on Python 3.7.
---
+++ startec2.py
@@ -0,0 +1,45 @@
+import json
+import boto3
+import os
+
+region = os.environ['AWS_REGION']
+
+def lambda_handler(event, context):
+    
+    client = boto3.client('ssm')
+    
+    response = client.start_automation_execution(
+        DocumentName='AWS-StartEC2Instance',
+        DocumentVersion='$DEFAULT',
+        TargetParameterName='InstanceId',
+        Targets=[
+            {
+                'Key': 'ResourceGroup',
+                'Values': [
+                    'DemoInstances',
+                ]
+            },
+        ],
+        MaxConcurrency='100%',
+        MaxErrors='2',
+        TargetLocations=[
+        {
+            'Accounts': [
+                '883779074323',
+            ],
+            'Regions': [
+                region,
+            ],
+            'TargetLocationMaxConcurrency': '100%',
+            'TargetLocationMaxErrors': '2',
+            'ExecutionRoleName': 'SSMAutomationRole'
+        }
+        ]
+    )
+
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('It Worked!')
+    }
+
